bd19944ce915019c08c6b461afe878f3c0a4a445
Samar Panda <samar.panda84@gmail.com>
2015-09-01 07:58:15 +0530

    Removed console logs from thumbnailSpec.js

--- test/js/thumbnailSpec.js
+++ test/js/thumbnailSpec.js
@@ -3,13 +3,11 @@ import Thumbnail from '../../app/js/thumbnail.js';
 describe('thumbnail.js test', function(){
 	let th;
 	beforeEach(()=>{
-		console.log("Before");
 		this.th = null;
 	});
 
-	afterEach(()=>{
-		console.log("After");
-	});
+	afterEach(()=>{});
+
 	it('Create Object of thumbnail', () => {
 		this.th = new Thumbnail(1);
 		expect(this.th.getId()).toEqual(1);
